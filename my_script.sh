b779093b27ff8e9e50237c5af0e573dfdfa08bd1
sebastian villegas <villegassebastian415@gmail.com>
2026-06-05 14:30:47 -0500

    update

--- my_script.sh
+++ my_script.sh
@@ -14,6 +14,8 @@ mkdir -p /root/bot_files/storage
 mkdir -p /root/storage
 mkdir -p /root/bot_files/logs
 mkdir -p /root/bot_files/screenshots
+mkdir -p /root/bot_files/chrome_profile
+chmod -R 777 /root/bot_files/chrome_profile
 cd /root/bot_files
 
 # ========== INSTALAR GOOGLE CHROME ==========
@@ -33,10 +35,12 @@ fi
 # Verificar instalación
 echo "✅ Google Chrome $(google-chrome --version) instalado"
 
+sudo apt-get install -y python3-tk python3-dev
 # Instalar Xvfb individualmente para evitar que un error detenga todo
-echo "Instalando Xvfb y librerías gráficas..."
+#echo "Instalando Xvfb y librerías gráficas..."
 apt install -y xvfb || echo "⚠️ Xvfb instalado con advertencias"
 
+echo "Instaland librerías gráficas..."
 # ========== INSTALAR LIBRERÍAS GRÁFICAS ==========
 apt install -y \
     libxss1 \
@@ -65,7 +69,7 @@ apt install -y \
 # Configurar zona horaria
 timedatectl set-timezone America/Bogota
 
-# ========== CONFIGURAR DISPLAY VIRTUAL ==========
+#========== CONFIGURAR DISPLAY VIRTUAL ==========
 echo "Configurando Xvfb como servicio systemd..."
 
 cat > /etc/systemd/system/xvfb.service << 'EOF'
@@ -75,7 +79,7 @@ After=network.target
 Before=betting-bot.service
 
 [Service]
-ExecStart=/usr/bin/Xvfb :99 -screen 0 1280x1024x24 -ac +extension GLX +render -noreset
+ExecStart=/usr/bin/Xvfb :99 -screen 0 1920x1080x24 -ac +extension GLX +render -noreset
 Restart=always
 User=root
 
@@ -83,13 +87,41 @@ User=root
 WantedBy=multi-user.target
 EOF
 
-# Habilitar e iniciar Xvfb
+#Habilitar e iniciar Xvfb
 systemctl daemon-reload
 systemctl enable xvfb.service
 systemctl start xvfb.service
 
 # Esperar a que Xvfb esté listo
 sleep 2
+
+#========== CONFIGURAR LOGROTATE ==========
+echo "⚙️ Configurando logrotate para el Bot..."
+
+# Asegurar que la herramienta logrotate esté instalada
+apt install -y logrotate
+
+cat > /etc/logrotate.d/betting-bot << 'EOF'
+/var/log/betting-bot.log
+/var/log/betting-bot-error.log
+{
+    rotate 7
+    daily
+    maxsize 100M
+    dateext
+    compress
+    delaycompress
+    missingok
+    notifempty
+    create 0640 root root
+    postrotate
+        systemctl restart betting-bot.service > /dev/null 2>&1 || true
+    endscript
+}
+EOF
+
+#Ajustar permisos correctos para el archivo de configuración
+chmod 644 /etc/logrotate.d/betting-bot
 
 echo "=========================================="
 echo "✅ Instalación base completada"
